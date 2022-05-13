8d8efb88ba5528ac3abb7399281c3e047064d5b3
rajib074 <rajib01711173241@gmail.com>
2022-05-13 17:25:35 +0600

    product dada add

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const express = require('express')
-var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+var cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxgqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("rajib").collection("productCollection");
-  console.log('rajib is ok');
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db('data').collection('product');
+
+
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products)
+    });
+
+    app.get('/product/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query={_id: ObjectId(id)};
+      const product = await productCollection.findOne(query);
+      res.send(product);
+  });
+
+  } 
+  
+  finally {
+
+
+  }
+}
+run().catch(console.dir);
 
 
 
-
-app.get('/', (req, res) => {
-  res.send('Hello all World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello all World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
